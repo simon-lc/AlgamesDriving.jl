5650998bc145e71af504ce0efe676fe8acf5f7b0
simon-lc <simonlc@stanford.edu>
2021-05-30 15:20:39 -0700

    fixing AuRo exp

--- autonomous_robots_figures/figure_5a_gne_subspace_continuum.jl
+++ autonomous_robots_figures/figure_5a_gne_subspace_continuum.jl
@@ -1,3 +1,4 @@
+using Plots
 include("experiment_helpers.jl")
 
 # Create Roadway
@@ -104,6 +105,13 @@ function get_figure(prob::Algames.GameProblem)
 	ascore = ActiveSetCore(probsize)
 	update_nullspace!(ascore, prob, prob.pdtraj)
 	ns = length(ascore.null.vec)
+	N = prob.probsize.N
+	n = prob.probsize.n
+	m = prob.probsize.m
+	p = prob.probsize.p
+	S = prob.probsize.S
+	Sh = S + p*(p-1)*(N-1)
+	Sv = S + Int(p*(p-1)*(N-1)/2)
 	x0 = get_primal_dual(ascore, prob)
 	v0 = deepcopy(ascore.null.vec[2])
 
@@ -127,7 +135,7 @@ function get_figure(prob::Algames.GameProblem)
 
 		plot_traj_!(model, prob.pdtraj.pr, plt=plt)
 		prob_c = deepcopy(prob)
-		set_primal_dual!(ascore, prob_c, x+α*v)
+		set_primal_dual!(ascore, prob_c, x+α*v0)
 	end
 	display(plt)
 
@@ -145,8 +153,102 @@ prob_copy = deepcopy(prob)
 prob_copy, pdtrajs = get_figure(prob_copy)
 
 for (k,pdtraj) in enumerate(pdtrajs[[1,20,40,60,80,100]])
-	vis_opts = LineVisualizationOptions(line_width=3.0, α=1.0)
+	vis_opts = LineVisualizationOptions(line_width=2.0, α=1.0)
 	set_line_traj!(vis, model, sce.player, pdtraj.pr, vis_opts=vis_opts, key=k)
 end
 
 settransform!(vis["env/roadway"], Translation(0.0, -0.05, 0.0))
+
+
+
+
+
+
+# for (k,pdtraj) in enumerate(pdtrajs[[(1:1:80); (80:-1:1)]])
+for (k,pdtraj) in enumerate(pdtrajs[[(1:1:80); (80:-1:1)]])
+	set_traj!(vis, model, sce, pdtraj.pr[end:end])
+	set_line_traj!(vis, model, sce.player, pdtraj.pr, vis_opts=LineVisualizationOptions(line_width=8.0))
+	sleep(0.3)
+	save_image(vis)
+end
+
+prob.pdtraj.pr
+
+[(1:5:100);]
+[(1:5:100);]
+plot([(1:5:100); (100:-5:1)])
+
+
+using FFMPEG
+using MeshCat
+filename = "GNE_continuum"
+MeshCat.convert_frames_to_video(
+# convert_frames_to_video(
+    "/home/simon/Downloads/$filename.tar",
+    "/home/simon/Documents/$filename.mp4", overwrite=true)
+
+convert_video_to_gif(
+    "/home/simon/Documents/$filename.mp4",
+    "/home/simon/Documents/$filename.gif", overwrite=true)
+
+unpack_cmd = MeshCat.unpack_cmd
+using FFMPEG
+
+function convert_frames_to_video(tar_file_path::AbstractString, output_path::AbstractString="output.mp4"; framerate=60, overwrite=false)
+    output_path = abspath(output_path)
+    if !isfile(tar_file_path)
+        error("Could not find the input file $tar_file_path")
+    end
+    if isfile(output_path) && !overwrite
+        error("The output path $output_path already exists. To overwrite that file, you can pass `overwrite=true` to this function")
+    end
+
+    mktempdir() do tmpdir
+        run(unpack_cmd(tar_file_path, tmpdir, ".zip", nothing))
+        cmd = ["-r", string(framerate), "-i", "*.png", "-vcodec", "libx264", "-preset", "slow", "-crf", "18"]
+        if overwrite
+            push!(cmd, "-y")
+        end
+        push!(cmd, output_path)
+
+        cd(tmpdir) do
+            FFMPEG.exe(cmd...)
+        end
+    end
+    @info("Saved output as $output_path")
+    return output_path
+end
+
+
+
+
+function convert_video_to_gif(video_file_path::AbstractString, output_path::AbstractString="output.gif";
+    framerate::Int=30, start_time=0., duration=1e3, overwrite=false, width::Int=1080, height::Int=-2, hq_colors::Bool=false)
+    output_path = abspath(output_path)
+
+    if !isfile(video_file_path)
+        error("Could not find the input file $video_file_path")
+    end
+    if isfile(output_path) && !overwrite
+        error("The output path $output_path already exists. To overwrite that file, you can pass `overwrite=true` to this function")
+    end
+
+    mktempdir() do tmpdir
+        # run(MeshCat.unpack_cmd(video_file_path, tmpdir, ".mp4", nothing)) # unpack the .tar file
+        # cmd = ["-r", string(framerate), "-i", "%07d.png", "-vcodec", "libx264", "-preset", "slow", "-crf", "18"]
+        color_map = hq_colors ?
+            "[0:v] fps=$framerate, scale=$width:$height,split [a][b];[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1" :
+            "[0:v] fps=$framerate, scale=$width:$height,split [a][b];[a] palettegen [p];[b][p] paletteuse"
+        cmd = ["-ss", string(start_time), "-t", string(duration), "-i", video_file_path, "-filter_complex", color_map]
+        if overwrite
+            push!(cmd, "-y")
+        end
+        push!(cmd, output_path)
+
+        cd(tmpdir) do
+            FFMPEG.exe(cmd...)
+        end
+    end
+    @info("Saved output as $output_path")
+    return output_path
+end
